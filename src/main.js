e8c41b1d6645ff40a5dda00e9e01f05531472d00
etienne <laurentlionel20@hotmail.com>
2025-06-07 20:43:08 +0200

    [WIP] scene creation

--- src/main.js
+++ src/main.js
@@ -2,34 +2,43 @@ import * as THREE from 'three';
 import "./style.css"
 import {QuitGameButton} from "./components/Button/Button.js"
 
+// Setup scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight, 
+  0.1, 
+  1000
+);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const renderer = new THREE.WebGLRenderer()
 
-camera.position.set( 0, 0, 10 );
-camera.lookAt( 0, 0, 0 );
+const geometry = new THREE.PlaneGeometry( 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry, material );
+scene.add( plane );
 
-const mat = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+//board.rotation.x = -Math.PI / 2; // Lay flat
 
-const geo = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( geo, mat );
-scene.add( line );
+/*
+// Create a card
+const card = new THREE.Mesh(
+  new THREE.BoxGeometry(0.0635, 0.003, 0.0889),
+  new THREE.MeshStandardMaterial({ 
+    color: 0x505050
+  })
+);
+card.position.set(0, 0.002, 0); // Slightly above board
+scene.add(card);
+*/
+// Position camera
+camera.position.set(1, 1, 2);
+camera.lookAt(0, 0, 0);
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 function animate() {
-cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
   renderer.render( scene, camera );
 }
 
@@ -37,4 +46,7 @@ cube.rotation.y += 0.01;
 renderer.setAnimationLoop( animate );
 
 const quitButton = new QuitGameButton(document.body)
+quitButton.addEventListener("click",()=>{
+  console.log("quit")
+})
 quitButton.render()
